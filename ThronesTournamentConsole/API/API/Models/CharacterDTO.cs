3cd3ee21839a1d5d1cbd6a1795a612d77fe35e37
CHEDOTAL Aubin <aubin.chedotal@poste.isima.fr>
2018-02-02 15:04:38 +0100

    adding API files

---
+++ ThronesTournamentConsole/API/API/Models/CharacterDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class CharacterDTO
+    {
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public int Pv { get; set; }
+
+        public CharacterDTO()
+        {
+        }
+
+        public CharacterDTO(string nom, string prenom, int pv)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Pv = pv;
+        }
+    }
+}
